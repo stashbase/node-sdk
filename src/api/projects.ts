91e38d09e97be5d23edad55351f4f5993587bce1
dimak <rahbussiness@gmail.com>
2023-09-22 10:51:37 +0200

    projects api

--- src/api/projects.ts
+++ src/api/projects.ts
@@ -1,30 +1,16 @@
 import { AxiosInstance } from 'axios'
 import { EnvironmentsAPI } from './environments'
 
-interface Project {
-  id: number
-  name: string
-  // Other project properties
-}
-
-export class ProjectsAPI {
-  private _environmentsAPI: EnvironmentsAPI | null = null
-
-  constructor(private readonly client: AxiosInstance) {}
-
-  async list(): Promise<Project[]> {
-    try {
-      const response = await this.client.get<Project[]>('/projects')
-      return response.data
-    } catch (error) {
-      throw error
-    }
+function projectsAPI(client: AxiosInstance, accessToken: string) {
+  // Define methods that use the project name and access token
+  function list(): any {
+    return accessToken
+    // return listEnvironmentsInternal(client, projectName)
   }
 
-  environments(args: { project: string }): EnvironmentsAPI {
-    if (!this._environmentsAPI) {
-      this._environmentsAPI = new EnvironmentsAPI(this.client, args)
-    }
-    return this._environmentsAPI
+  return {
+    list,
   }
 }
+
+export default projectsAPI
