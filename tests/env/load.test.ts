2149691fa9d4eb6446615179c011dea75c5553e4
Radim Höfer <radim.engineering@stashbase.dev>
2023-09-22 11:15:45 +0200

    test load env

---
+++ tests/env/load.test.ts
@@ -0,0 +1,19 @@
+// tests/projects.test.ts
+import { test } from 'vitest'
+import { createEnvEase } from '../../src'
+
+test('Load env and inject the variables', async () => {
+  const envEase = createEnvEase('AccessToken')
+
+  // const projects = envEase.projects.list()
+  // console.log(projects)
+  await envEase.projects.environments.load({
+    project: 'hero-hub-api',
+    environment: 'local',
+    options: {
+      printTable: true,
+    },
+  })
+
+  console.log(process.env.DATABASE_URL)
+})
