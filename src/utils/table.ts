68bad984370a03d8e847a0c583a4a6e1d8b727a7
Radim Höfer <radim.engineering@stashbase.dev>
2023-09-30 17:51:10 +0200

    print loaded env table fn

---
+++ src/utils/table.ts
@@ -0,0 +1,18 @@
+import Table from 'cli-table3'
+
+export function printLoadedEnvTable(envSecrets: Record<string, string>) {
+  const table = new Table({
+    head: ['Key', 'Value'],
+    // colWidths: [40, 40],
+    wordWrap: true,
+    style: {
+      compact: true,
+    },
+  })
+
+  for (const [key, value] of Object.entries(envSecrets)) {
+    table.push([key, value])
+  }
+
+  console.log(table.toString())
+}
