16a355d06e01f3936e48cc7d3f7422bf6fd50ea6
dimak <rahbussiness@gmail.com>
2023-09-30 17:50:54 +0200

    test

--- tests/env/load.test.ts
+++ tests/env/load.test.ts
@@ -1,21 +1,34 @@
 // tests/projects.test.ts
 import { assert, test } from 'vitest'
-import { createEnvEase } from '../../src'
+import { createEnvApi } from '../../src'
 
-test('Load env and inject the variables', async () => {
-  const envEase = createEnvEase('AccessToken')
+test('Load specific env with env token and inject the variables into the process', async () => {
+  const envApi = createEnvApi('-YSbVSq5kzmc1EgzAO9SThUXFQLjb6R1')
 
-  // const projects = envEase.projects.list()
-  // console.log(projects)
-  await envEase.projects.environments.load({
-    project: 'hero-hub-api',
-    environment: 'local',
-    options: {
-      printTable: true,
-    },
+  await envApi.load({
+    printTable: true,
+    shouldThrow: true,
   })
 
-  console.log(process.env.DATABASE_URL)
-
-  assert.exists(process.env.DATABASE_URL)
+  console.log(process.env.JWT_SECRET_KEY)
+  assert.exists(process.env.JWT_SECRET_KEY)
 })
+
+// ROOT
+// test('Load env and inject the variables', async () => {
+//   const envEase = createEnvEase('AccessToken')
+//
+//   // const projects = envEase.projects.list()
+//   // console.log(projects)
+//   await envEase.projects.environments.load({
+//     project: 'hero-hub-api',
+//     environment: 'local',
+//     options: {
+//       printTable: true,
+//     },
+//   })
+//
+//   console.log(process.env.DATABASE_URL)
+//
+//   assert.exists(process.env.DATABASE_URL)
+// })
