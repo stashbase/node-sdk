8748b6812f411da58e89acdf872ee8733bb880ab
Radim Höfer <radim@stashbase.dev>
2023-09-30 17:59:14 +0200

    get env

--- src/api/environments/api.ts
+++ src/api/environments/api.ts
@@ -1,9 +1,11 @@
 import { AxiosInstance } from 'axios'
 import { LoadEnvironmentOpts, loadEnvironment } from './handlers/load'
+import { GetEnvironmentOpts, getEnvironment } from './handlers/get'
 
 // access only selected environment with environment token
 // client with env token
 function environmentsAPI(envClient: AxiosInstance) {
+  // load and inject environment variables
   async function load(options?: LoadEnvironmentOpts) {
     if (options?.enabled === false) {
       return
@@ -12,8 +14,13 @@ function environmentsAPI(envClient: AxiosInstance) {
     return await loadEnvironment(envClient, options)
   }
 
+  async function get(options?: GetEnvironmentOpts) {
+    return await getEnvironment(envClient, options)
+  }
+
   return {
     load,
+    get,
   }
 }
 
