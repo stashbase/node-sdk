caa5a148e615a747a88f67cf34330b905bdf6fe1
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-01 12:24:46 +0200

    use http client

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,23 @@
 import environmentsAPI from './api/environments/api'
 import { createEnvApiClient } from './api/environments/client'
-import { createApiClient } from './oldApi/client'
-import projectsAPI from './oldApi/projects'
+import { createHttpClient } from './http/client'
 
 // Create an SDK object that encapsulates functionality
 // ROOT
 export function createEnvEase(accessToken: string) {
-  const client = createApiClient(accessToken)
-
-  const projects = projectsAPI(client)
-
-  return {
-    projects,
-  }
+  //   const client = (accessToken)
+  //
+  //   const projects = projectsAPI(client)
+  //
+  //   return {
+  //     projects,
+  //   }
 }
 
 // only for env with env token
 export function createEnvApi(envToken: string) {
   console.log(envToken)
-  const client = createEnvApiClient(envToken)
+  const client = createHttpClient({ basePath: 'environments', authorization: { envToken } })
 
   return environmentsAPI(client)
 }
