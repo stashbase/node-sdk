572bf43bbe1b11ccf6f2d2189677867ff08b4d36
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-01 15:22:32 +0200

    args

--- tests/env/get.test.ts
+++ tests/env/get.test.ts
@@ -5,15 +5,15 @@ import { createEnvApi } from '../../src'
 test('Get specific env with env token and return variables with name', async () => {
   const envApi = createEnvApi('-YSbVSq5kzmc1EgzAO9SThUXFQLjb6R1')
 
-  const { data, error } = await envApi.get({
-    printTable: false,
-  })
+  const { data, error } = await envApi.get({})
 
   assert.equal(error, null)
 
   if (data) {
+    console.log(data)
+
     const { name, secrets } = data
     console.log(name)
     console.log(secrets)
   }
-})
+} )
