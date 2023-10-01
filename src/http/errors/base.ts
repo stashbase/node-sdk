d0c4e7f2b7ff424ab6f62a878560080f7b8d6933
Radim Höfer <radim@stashbase.dev>
2023-10-01 15:54:21 +0200

    bad request error

--- src/http/errors/base.ts
+++ src/http/errors/base.ts
@@ -1,6 +1,15 @@
 import { ApiError } from '../response'
 
 export function createApiErrorFromResponse<T>(responseData: any): T {
+  // validation error = bad request
+  if (
+    responseData &&
+    responseData?.code === 'FST_ERR_VALIDATION' &&
+    responseData?.statusCode === 400
+  ) {
+    return <T>{ code: 'bad_request' }
+  }
+
   if (responseData && responseData.error) {
     return <T>{ code: responseData.error.code }
   }
