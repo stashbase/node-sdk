19dd65b9cc95f53485119ffd324572941ae711ac
Radim Höfer <radim@stashbase.dev>
2023-10-01 17:39:52 +0200

    patch request

--- src/http/client.ts
+++ src/http/client.ts
@@ -2,9 +2,12 @@ const baseURL: string = 'http://0.0.0.0:8080/api/v1/sdk/'
 
 type BasePath = 'environments' | 'projects'
 
+type RequestWithData = { path: string; data: { [key: string]: any } | any[] }
+
 export type HttpClient = {
   get: <T>(args: { path: string; query?: { [key: string]: string } }) => Promise<T>
-  post: <T>(args: { path: string; data: { [key: string]: any } | any[] }) => Promise<T>
+  post: <T>(args: RequestWithData) => Promise<T>
+  patch: <T>(args: RequestWithData) => Promise<T>
 }
 
 export function createHttpClient(args: {
@@ -67,42 +70,71 @@ export function createHttpClient(args: {
   }
 
   async function post<T>(args: { path: string; data: { [key: string]: any } | any[] }): Promise<T> {
-    const { path, data } = args
+    return await postOrPatch<T>({
+      method: 'POST',
+      headers,
+      basePath,
+      path: args.path,
+      data: args.data,
+    })
+  }
 
-    const url = `${baseURL}${basePath}${path ?? ''}`
-
-    try {
-      const response = await fetch(url, {
-        method: 'POST',
-        headers,
-        body: JSON.stringify(data),
-      })
-
-      if (!response.ok) {
-        if (response.status === 500 || response.status === 404) {
-          throw new Error('Internal Server Error')
-        } else {
-          // TODO: errors
-          const errorData = await response.json() // Parse error response
-          console.error(errorData)
-          throw errorData // Throw the entire error response object
-        }
-      }
-
-      const responseData = await response.json()
-      return responseData as T
-    } catch (error) {
-      if (error instanceof Error) {
-        console.error('A network-related error occurred:', error.message)
-        throw error
-      } else {
-        throw error
-      }
-    }
+  async function patch<T>(args: {
+    path: string
+    data: { [key: string]: any } | any[]
+  }): Promise<T> {
+    return await postOrPatch<T>({
+      method: 'PATCH',
+      headers,
+      basePath,
+      path: args.path,
+      data: args.data,
+    })
   }
 
   return {
     get,
     post,
+    patch,
+  }
+}
+
+async function postOrPatch<T>(args: {
+  method: 'POST' | 'PATCH'
+  headers: Record<string, string>
+  basePath: string
+  path: string
+  data: { [key: string]: any } | any[]
+}): Promise<T> {
+  const { method, basePath, headers, path, data } = args
+
+  const url = `${baseURL}${basePath}${path ?? ''}`
+
+  try {
+    const response = await fetch(url, {
+      method,
+      headers,
+      body: JSON.stringify(data),
+    })
+
+    if (!response.ok) {
+      if (response.status === 500 || response.status === 404) {
+        throw new Error('Internal Server Error')
+      } else {
+        const errorData = await response.json() // Parse error response
+        console.error(errorData)
+        throw errorData // Throw the entire error response object
+      }
+    }
+
+    const responseData = await response.json()
+    return responseData as T
+  } catch (error) {
+    if (error instanceof Error) {
+      console.error('A network-related error occurred:', error.message)
+      throw error
+    } else {
+      throw error
+    }
   }
 }
