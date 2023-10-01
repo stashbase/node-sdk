db31389113f9f95929a6e8375be05f427237f87b
dimak <rahbussiness@gmail.com>
2023-10-01 17:39:57 +0200

    update secrets

---
+++ src/api/environments/handlers/secrets/update.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '../../../../http/client'
+import { ApiError, ApiResponse } from '../../../../http/response'
+import { createApiErrorFromResponse } from '../../../../http/errors/base'
+import { SecretsApiError } from '../../errors/secrets'
+
+type UpdateSecretsResponseData = {
+  updatedCount: number
+  notFoundKeys?: string[]
+}
+
+type UpdateSecretsError = ApiError<SecretsApiError | 'no_values_provided' | 'missing_properties'>
+
+export type UpdateSecretsData = Array<{
+  key: string
+  newKey?: string
+  value?: string
+  description?: string | null
+}>
+
+async function updateSecrets(
+  envClient: HttpClient,
+  data: UpdateSecretsData
+): Promise<ApiResponse<UpdateSecretsResponseData, UpdateSecretsError>> {
+  try {
+    const secrets = await envClient.patch<UpdateSecretsResponseData>({
+      path: '/secrets',
+      data,
+    })
+
+    return { data: secrets, error: null }
+  } catch (error: any) {
+    console.log('Error: ', error?.error)
+    const apiError = createApiErrorFromResponse<UpdateSecretsError>(error)
+
+    return { data: null, error: apiError }
+  }
+}
+
+export { updateSecrets }
