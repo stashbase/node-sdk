b52fa1d72afcc5395528891c22b28c28c95885d9
dimak <rahbussiness@gmail.com>
2023-10-01 21:11:24 +0200

    commented axios fn

--- src/api/environments/client.ts
+++ src/api/environments/client.ts
@@ -1,14 +1,14 @@
-import axios, { AxiosInstance } from 'axios'
-
-const baseURL: string = 'http://0.0.0.0:8080/api/v1/sdk/environments'
-
-export function createEnvApiClient(envToken: string): AxiosInstance {
-  return axios.create({
-    baseURL,
-    headers: {
-      Authorization: `EnvToken ${envToken}`,
-      'Content-Type': 'application/json',
-      'User-Agent': 'EnvEase SDK/0.0.1',
-    },
-  })
-}
+// import axios, { AxiosInstance } from 'axios'
+//
+// const baseURL: string = 'http://0.0.0.0:8080/api/v1/sdk/environments'
+//
+// export function createEnvApiClient(envToken: string): AxiosInstance {
+//   return axios.create({
+//     baseURL,
+//     headers: {
+//       Authorization: `EnvToken ${envToken}`,
+//       'Content-Type': 'application/json',
+//       'User-Agent': 'EnvEase SDK/0.0.1',
+//     },
+//   })
+// }
