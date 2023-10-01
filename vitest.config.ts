ce23873ccf5fbfe726f4117c5ce42f240c2fb1e4
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-01 21:14:25 +0200

    vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,11 @@
-// vitest.config.js
-export default {
-  testDir: 'tests',
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    dir: 'tests',
+    testTimeout: 10000,
+  },
   server: {
     port: 3000,
   },
-}
+})
