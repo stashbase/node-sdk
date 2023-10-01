9e384c64e45a4dbb4eef78c1f904a43a5113c1b3
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-01 15:54:42 +0200

    creat secrets test

---
+++ tests/env/secrets/create.test.ts
@@ -0,0 +1,26 @@
+import { test } from 'vitest'
+import { createEnvApi } from '../../../src'
+
+test('Create new secrets (add them to environment)', async () => {
+  const envApi = createEnvApi('-YSbVSq5kzmc1EgzAO9SThUXFQLjb6R1')
+
+  const { data, error } = await envApi.secrets.create([
+    {
+      key: 'Some key',
+      value: 'Some value',
+      description: 'This is description for this and this',
+    },
+  ])
+  if (error) {
+    const { code } = error
+    console.log('Error: ', error)
+    console.log(code)
+  } else {
+    console.log('Data:\n')
+    console.log(data)
+
+    const { createdCount, duplicateKeys } = data
+    console.log('createdCount: ', createdCount)
+    console.log('duplicateKeys: ', duplicateKeys)
+  }
+})
