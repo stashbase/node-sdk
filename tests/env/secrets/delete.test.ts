9706c1c2c04aad7b8effa313f52edc001b30d088
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-01 14:18:55 +0200

    test delete secret

--- tests/env/secrets/delete.test.ts
+++ tests/env/secrets/delete.test.ts
@@ -4,7 +4,13 @@ import { createEnvApi } from '../../../src'
 test('Delete specific secret from env - grant must be write or read/write', async () => {
   const envApi = createEnvApi('-YSbVSq5kzmc1EgzAO9SThUXFQLjb6R1')
 
-  const { notFound } = await envApi.secrets.remove(['123'])
-
-  console.log(notFound)
+  const { data, error } = await envApi.secrets.remove(['NEXT_PUBLIC_PRODUCTION_GITHUB_URL'])
+  if (error) {
+    const { code } = error
+    console.log(code)
+  } else {
+    const { deletedCount, notFound } = data
+    console.log('deletedCount: ', deletedCount)
+    console.log('notFound: ', notFound)
+  }
 })
