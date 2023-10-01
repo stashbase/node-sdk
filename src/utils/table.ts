707996acbb1e7c6fd17246204ffbc14c0c33fad8
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-01 15:22:19 +0200

    args

--- src/utils/table.ts
+++ src/utils/table.ts
@@ -1,6 +1,11 @@
 import Table from 'cli-table3'
 
-export function printLoadedEnvTable(envSecrets: Record<string, string>) {
+export function printSecretsTable(args: {
+  secretsObj?: Record<string, string>
+  array?: Array<{ key: string; value: string }>
+}) {
+  const { array, secretsObj } = args
+
   const table = new Table({
     head: ['Key', 'Value'],
     // colWidths: [40, 40],
@@ -11,8 +16,14 @@ export function printLoadedEnvTable(envSecrets: Record<string, string>) {
     },
   })
 
-  for (const [key, value] of Object.entries(envSecrets)) {
-    table.push([key, value])
+  if (secretsObj) {
+    for (const [key, value] of Object.entries(secretsObj)) {
+      table.push([key, value])
+    }
+  } else if (array) {
+    for (const { key, value } of array) {
+      table.push([key, value])
+    }
   }
 
   console.log(table.toString())
