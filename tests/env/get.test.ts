c8a7e0c76a6fd4123e2e8c2b3e80f8dc13e57ef1
dimak <rahbussiness@gmail.com>
2023-10-01 13:51:40 +0200

    tests

--- tests/env/get.test.ts
+++ tests/env/get.test.ts
@@ -3,15 +3,15 @@ import { assert, test } from 'vitest'
 import { createEnvApi } from '../../src'
 
 test('Get specific env with env token and return variables with name', async () => {
-  const envApi = createEnvApi('')
+  const envApi = createEnvApi('-YSbVSq5kzmc1EgzAO9SThUXFQLjb6R1')
 
   const { data, error } = await envApi.get({
     printTable: false,
   })
-  if (error) {
-    console.log('Got an error')
-    console.log(error.code)
-  } else {
+
+  assert.equal(error, null)
+
+  if (data) {
     const { name, secrets } = data
     console.log(name)
     console.log(secrets)
---
+++ tests/env/get/error.test.ts
@@ -0,0 +1,21 @@
+// tests/projects.test.ts
+import { assert, test } from 'vitest'
+import { createEnvApi } from '../../../src'
+
+test('Test unauthorized error', async () => {
+  const envApi = createEnvApi('')
+
+  const { data, error } = await envApi.get({
+    printTable: false,
+  })
+  if (error) {
+    console.log('Got an error')
+    console.log(error.code === 'unauthorized')
+
+    assert.equal(error.code, 'unauthorized')
+  } else {
+    const { name, secrets } = data
+    console.log(name)
+    console.log(secrets)
+  }
+})
