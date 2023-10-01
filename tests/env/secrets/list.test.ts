6d125fcf69cb17cf440600949228e17e7575458c
dimak <rahbussiness@gmail.com>
2023-10-01 14:39:15 +0200

    list secrets test

---
+++ tests/env/secrets/list.test.ts
@@ -0,0 +1,15 @@
+import { test } from 'vitest'
+import { createEnvApi } from '../../../src'
+
+test('List environment secrets', async () => {
+  const envApi = createEnvApi('-YSbVSq5kzmc1EgzAO9SThUXFQLjb6R1')
+
+  const { data, error } = await envApi.secrets.list({ description: true })
+  if (error) {
+    const { code } = error
+    console.log(code)
+  } else {
+    console.log('Data:\n')
+    console.log(data)
+  }
+})
