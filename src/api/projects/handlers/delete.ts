f59d0e4f0d283550a5af24abea59c67f5a041a90
dimak <rahbussiness@gmail.com>
2023-10-02 20:18:35 +0200

    delete projects

---
+++ src/api/projects/handlers/delete.ts
@@ -0,0 +1,32 @@
+import { HttpClient } from '../../../http/client'
+import { createApiErrorFromResponse } from '../../../http/errors/base'
+import { ApiError, ApiResponse } from '../../../http/response'
+
+type DeleteProjectsError = ApiError
+
+type DeleteProjectsResponseData = {
+  deletedCount: number
+  notFound?: string[]
+}
+
+async function deleteProjects(
+  client: HttpClient,
+  names: string[]
+): Promise<ApiResponse<DeleteProjectsResponseData, DeleteProjectsError>> {
+  try {
+    const data = await client.post<DeleteProjectsResponseData>({
+      path: 'projects/delete',
+      data: {
+        names,
+      },
+    })
+
+    return { data, error: null }
+  } catch (error: any) {
+    const apiError = createApiErrorFromResponse<DeleteProjectsError>(error)
+
+    return { data: null, error: apiError }
+  }
+}
+
+export { deleteProjects }
