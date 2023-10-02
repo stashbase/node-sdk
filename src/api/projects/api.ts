4763200ca96ce4ec67b878c15844e07cde11945f
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-02 18:45:27 +0200

    create project

--- src/api/projects/api.ts
+++ src/api/projects/api.ts
@@ -1,4 +1,5 @@
 import { HttpClient } from '../../http/client'
+import { CreateProjectData, createProject } from './handlers/create'
 import { getProject } from './handlers/get'
 import { ListProjectsOpts, listProjects } from './handlers/list'
 
@@ -23,8 +24,19 @@ export function projectsAPI(httpClient: HttpClient) {
     return await listProjects(httpClient, options)
   }
 
+  /**
+   * @summary Create new project
+   * @description Project
+   * @param data Project input
+   * @returns null
+   * */
+  async function create(data: CreateProjectData) {
+    return await createProject(httpClient, data)
+  }
+
   return {
     get,
     list,
+    create,
   }
 }
---
+++ src/api/projects/handlers/create.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '../../../http/client'
+import { createApiErrorFromResponse } from '../../../http/errors/base'
+import { ApiError, ApiResponse } from '../../../http/response'
+
+type CreateProjectResponseData = null
+
+type CreateSecretsError = ApiError<'already_exists'>
+
+export type CreateProjectData = {
+  name: string
+  description?: string
+}
+
+export async function createProject(
+  envClient: HttpClient,
+  data: CreateProjectData
+): Promise<ApiResponse<CreateProjectResponseData, CreateSecretsError>> {
+  try {
+    await envClient.post<CreateProjectResponseData>({
+      path: 'projects',
+      data,
+    })
+
+    return { data: null, error: null }
+  } catch (error: any) {
+    console.log('Error: ', error?.error)
+    const apiError = createApiErrorFromResponse<CreateSecretsError>(error)
+
+    return { data: null, error: apiError }
+  }
+}
