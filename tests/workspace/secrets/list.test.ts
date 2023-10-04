cc32608904eddfbfa5f3e2c3b53501854d05c124
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-04 13:27:14 +0200

    list secrets test

---
+++ tests/workspace/secrets/list.test.ts
@@ -0,0 +1,17 @@
+import { describe, test } from 'vitest'
+import { createEnvEase } from '../../../src'
+
+describe('List secrets', () => {
+  test('OK', async () => {
+    const envEase = createEnvEase('xPKDa2Xq0zWmfES1nLDoG45qZtR1z2qL')
+
+    const { data, error } = await envEase.secrets.list({
+      project: 'hero-hub',
+      name: 'vercel',
+      description: true,
+    })
+
+    console.log(data)
+    console.log(error)
+  })
+})
