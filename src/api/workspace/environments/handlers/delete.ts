f9039f24a19ef2719553d0f93acdae55ea7706b6
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-04 10:28:11 +0200

    delete environment

---
+++ src/api/workspace/environments/handlers/delete.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '../../../../http/client'
+import { ApiError, ApiResponse } from '../../../../http/response'
+import { createApiErrorFromResponse } from '../../../../http/errors/base'
+
+export interface DeleteEnvironmentArgs {
+  project: string
+  environment: string
+}
+
+type DeleteEnvironmentError = ApiError<'project_not_found' | 'environment_not_found'>
+
+async function deleteEnvironment(
+  client: HttpClient,
+  args: DeleteEnvironmentArgs
+): Promise<ApiResponse<null, DeleteEnvironmentError>> {
+  const { project } = args
+
+  try {
+    const data = await client.del<null>({
+      path: `/projects/${project}/environments/${args.environment}`,
+    })
+
+    return { data: data, error: null }
+  } catch (error: any) {
+    console.log(error)
+
+    const apiError = createApiErrorFromResponse<DeleteEnvironmentError>(error)
+
+    return { data: null, error: apiError }
+  }
+}
+
+export { deleteEnvironment }
