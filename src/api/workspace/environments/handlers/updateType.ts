80cf9be374d4e3fe9cf377c7aec950ca99669b00
Radim Höfer <radim.engineering@stashbase.dev>
2023-10-04 11:30:30 +0200

    update env type

---
+++ src/api/workspace/environments/handlers/updateType.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from '../../../../http/client'
+import { ApiError, ApiResponse } from '../../../../http/response'
+import { createApiErrorFromResponse } from '../../../../http/errors/base'
+
+export interface UpdateEnvironmentTypeArgs {
+  project: string
+  //
+  name: string
+  type: 'DEVELOPMENT' | 'TESTING' | 'STAGING' | 'PRODUCTION'
+}
+
+type UpdateEnvironmentTypeError = ApiError<
+  | 'project_not_found'
+  | 'environment_not_found'
+  | 'environment_already_exists'
+  | 'environment_locked'
+>
+
+async function updateEnvironmentType(
+  client: HttpClient,
+  args: UpdateEnvironmentTypeArgs
+): Promise<ApiResponse<null, UpdateEnvironmentTypeError>> {
+  const { project, name, type } = args
+
+  try {
+    const data = await client.patch<null>({
+      path: `/projects/${project}/environments/${name}/type`,
+      data: { type },
+    })
+
+    return { data: data, error: null }
+  } catch (error: any) {
+    const apiError = createApiErrorFromResponse<UpdateEnvironmentTypeError>(error)
+
+    return { data: null, error: apiError }
+  }
+}
+
+export { updateEnvironmentType }
