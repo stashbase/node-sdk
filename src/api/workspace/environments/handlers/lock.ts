9c3a3027de89e957d49e1c16d6fba7e60f2b9b34
Radim Höfer <radim@stashbase.dev>
2023-10-04 11:17:28 +0200

    lock/unlock env

---
+++ src/api/workspace/environments/handlers/lock.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '../../../../http/client'
+import { ApiError, ApiResponse } from '../../../../http/response'
+import { createApiErrorFromResponse } from '../../../../http/errors/base'
+
+export interface LockEnvironmentArgs {
+  project: string
+  // NOTE: naming ???
+  // environment: string
+  name: string
+}
+
+type LockEnvironmentError = ApiError<'project_not_found' | 'environment_not_found'>
+
+async function lockUnlockEnvironment(
+  client: HttpClient,
+  args: LockEnvironmentArgs,
+  lock: boolean
+): Promise<ApiResponse<null, LockEnvironmentError>> {
+  const { project, name } = args
+
+  try {
+    const data = await client.patch<null>({
+      path: `/projects/${project}/environments/${name}/${lock ? 'lock' : 'unlock'}`,
+    })
+
+    return { data: data, error: null }
+  } catch (error: any) {
+    console.log('Error: ', error?.error)
+    const apiError = createApiErrorFromResponse<LockEnvironmentError>(error)
+
+    return { data: null, error: apiError }
+  }
+}
+
+export { lockUnlockEnvironment }
