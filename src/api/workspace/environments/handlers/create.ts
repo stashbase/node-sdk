9516d2b5696360640d628d48f6ef6025570c180d
dimak <rahbussiness@gmail.com>
2023-10-04 10:16:57 +0200

    create environment

---
+++ src/api/workspace/environments/handlers/create.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '../../../../http/client'
+import { ApiError, ApiResponse } from '../../../../http/response'
+import { createApiErrorFromResponse } from '../../../../http/errors/base'
+
+export interface CreateEnvironmentArgs {
+  project: string
+  //
+  name: string
+  description?: string
+  type: 'DEVELOPMENT' | 'TESTING' | 'STAGING' | 'PRODUCTION'
+}
+
+type CreateEnvironmentError = ApiError<'project_not_found' | 'environment_already_exists'>
+
+async function createEnvironment(
+  client: HttpClient,
+  args: CreateEnvironmentArgs
+): Promise<ApiResponse<null, CreateEnvironmentError>> {
+  const { project } = args
+
+  try {
+    const data = await client.post<null>({
+      path: `/projects/${project}/environments`,
+      data: { ...args, project: undefined },
+    })
+
+    return { data: data, error: null }
+  } catch (error: any) {
+    const apiError = createApiErrorFromResponse<CreateEnvironmentError>(error)
+
+    return { data: null, error: apiError }
+  }
+}
+
+export { createEnvironment }
