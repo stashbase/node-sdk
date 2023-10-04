c7f88393893b19f27877d8a83cf135378093e75d
dimak <rahbussiness@gmail.com>
2023-10-04 10:28:29 +0200

    delete env test

---
+++ tests/workspace/environments/delete.test.ts
@@ -0,0 +1,32 @@
+import { assert, describe, test } from 'vitest'
+import { createEnvEase } from '../../../src'
+
+describe('Delete environment', () => {
+  test('Invalid token', async () => {
+    const envEase = createEnvEase('4r4')
+
+    const { data, error } = await envEase.environments.remove({
+      project: 'hero-hub1231231231',
+      environment: 'dev-sdk',
+    })
+
+    console.log(data)
+    console.log(error)
+
+    assert.equal(error?.code, 'invalid_token')
+  })
+
+  test('Project not found', async () => {
+    const envEase = createEnvEase('xPKDa2Xq0zWmfES1nLDoG45qZtR1z2qL')
+
+    const { data, error } = await envEase.environments.remove({
+      project: 'hero-hub1231231231',
+      environment: 'dev-sdk',
+    })
+
+    assert.equal(error?.code, 'project_not_found')
+
+    console.log(data)
+    console.log(error)
+  })
+})
