11b1e546c11c2675e176644a0654b0bd30a11618
dimak <rahbussiness@gmail.com>
2023-10-04 13:20:50 +0200

    get secret

---
+++ src/api/workspace/secrets/handlers/get.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '../../../../http/client'
+import { ApiError, ApiResponse } from '../../../../http/response'
+import { createApiErrorFromResponse } from '../../../../http/errors/base'
+
+type Secret = { key: string; value: string; description: string | null }
+
+type GetSecretError = ApiError<'project_not_found' | 'environment_not_found' | 'secret_not_found'>
+type GetSecretResponse = Promise<ApiResponse<Secret, GetSecretError>>
+
+export interface GetSecretArgs {
+  project: string
+  // environment name
+  name: string
+  // secret key
+  key: string
+}
+
+async function getSecret(envClient: HttpClient, args: GetSecretArgs): GetSecretResponse {
+  const { project, name, key } = args
+
+  try {
+    const secrets = await envClient.get<Secret>({
+      path: `/projects/${project}/environments/${name}/secrets/${key}`,
+    })
+
+    return { data: secrets, error: null }
+  } catch (error: any) {
+    console.log('Error: ', error?.error)
+    const apiError = createApiErrorFromResponse<GetSecretError>(error)
+
+    return { data: null, error: apiError }
+  }
+}
+
+export { getSecret }
