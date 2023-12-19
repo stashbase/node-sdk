1c0fe77eaf5e9490ee2cfb8290fa5c0345a676bd
Radim Höfer <radim.engineering@stashbase.dev>
2023-12-19 17:31:50 +0100

    description nullable

--- tests/env/secrets/set.test.ts
+++ tests/env/secrets/set.test.ts
@@ -2,16 +2,17 @@ import { test } from 'vitest'
 import { createEnvApi } from '../../../src'
 
 test('Set secrets (add new or update existing)', async () => {
-  const envApi = createEnvApi('V46rWNY1zh1NO2mFrtVqOnwlY4RTXti-')
+  const envApi = createEnvApi('mdkzKx0HXUCkwh2ERZzgbeOBRXOcPxFi')
 
   const { error } = await envApi.secrets.set([
     {
       key: 'SECRET',
       value: 'VALUE_123',
+      description: null,
     },
     {
-      key: 'AP_URL',
-      value: 'https://',
+      key: 'APPP_URL',
+      value: 'https://sss',
     },
   ])
   if (error) {
