83ac2801b6dc95436674e9bee24d8359ee19fa22
Radim Höfer <radim@stashbase.dev>
2023-12-19 21:16:14 +0100

    error details

--- src/http/errors/base.ts
+++ src/http/errors/base.ts
@@ -11,13 +11,13 @@ export function createApiErrorFromResponse<T>(responseData: {
     responseData?.code === 'FST_ERR_VALIDATION' &&
     responseData?.statusCode === 400
   ) {
-    return <T>{ code: 'bad_request' }
+    return <T>{ code: 'bad_request', details: responseData?.error?.details }
   }
 
   if (responseData && responseData.error) {
-    return <T>{ code: responseData.error.code }
+    return <T>{ code: responseData.error.code, details: responseData.error.details }
   }
 
-  // If no error data is present, return a default APIError
+  // If no error data is present, return a default APIErroR
   return <T>{ code: 'server_error' }
 }
