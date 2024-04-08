297818ce4db56a67ac1f18e8e0c8f568103500a2
dimak <rahbussiness@gmail.com>
2024-04-08 14:22:47 +0200

    feat: add + handle too_many_requests rate limit error

--- src/http/errors/base.ts
+++ src/http/errors/base.ts
@@ -14,6 +14,10 @@ export function createApiErrorFromResponse<T>(responseData: {
     return <T>{ code: 'bad_request' }
   }
 
+  if (responseData?.statusCode === 429) {
+    return <T>{ code: 'too_many_requests', details: responseData?.message ?? undefined }
+  }
+
   if (responseData && responseData.error) {
     return <T>{
       code: responseData?.error?.code,
