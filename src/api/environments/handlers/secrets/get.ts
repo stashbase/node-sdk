9487eb0596d5cc83810484a8eb0d9c3ad4ec6b2d
Radim Höfer <radim.engineering@stashbase.dev>
2024-07-17 13:36:39 +0200

    feat: add property + query "expandRefs"

--- src/api/environments/handlers/secrets/get.ts
+++ src/api/environments/handlers/secrets/get.ts
@@ -7,10 +7,15 @@ type Secret = { key: Uppercase<string>; value: string; description?: string }
 type GetSecretError = ApiError<'secret_not_found'>
 type GetSecretResponse = Promise<ApiResponse<Secret, GetSecretError>>
 
-async function getSecret(envClient: HttpClient, key: string): GetSecretResponse {
+async function getSecret(
+  envClient: HttpClient,
+  key: string,
+  expandRefs?: boolean
+): GetSecretResponse {
   try {
     const secrets = await envClient.get<Secret>({
       path: `/secrets/${key}`,
+      query: expandRefs ? { 'expand-refs': 'true' } : undefined,
     })
 
     return { data: secrets, error: null }
