a9779be0c4619fce0a2ffda1bc014378669045f7
Radim Höfer <radim@stashbase.dev>
2024-07-30 13:37:18 +0200

    refactor: secrets error details array type

--- src/types/errors/secrets.ts
+++ src/types/errors/secrets.ts
@@ -1,4 +1,3 @@
-import { SecretKey } from '../secretKey'
 import { ApiError, SharedApiErrorCode } from '../../http/response'
 
 export type CreateSecretsError =
@@ -25,17 +24,14 @@ export type UpdateSecretsError =
   | DuplicateNewSecretsError
   | SelfReferencingSecretsError
   | ApiError<'invalid_new_secret_keys', { newSecretKeys: Array<string> }>
-  | ApiError<'missing_properties_to_update', { secretKeys: Array<SecretKey> }>
-  | ApiError<'new_key_secrets_already_exist', { conflictingSecrets: Array<SecretKey> }>
+  | ApiError<'missing_properties_to_update', { secretKeys: Array<string> }>
+  | ApiError<'new_key_secrets_already_exist', { conflictingSecrets: Array<string> }>
 
 type NoValuesProvidedError = ApiError<'no_values_provided'>
 type InvalidSecretKeysError = ApiError<'invalid_secret_keys', { secretKeys: Array<string> }>
-type DuplicateSecretsError = ApiError<'duplicate_secrets', { duplicateSecrets: Array<SecretKey> }>
-type SelfReferencingSecretsError = ApiError<
-  'self_referencing_secrets',
-  { secrets: Array<SecretKey> }
->
+type DuplicateSecretsError = ApiError<'duplicate_secrets', { duplicateSecrets: Array<string> }>
+type SelfReferencingSecretsError = ApiError<'self_referencing_secrets', { secrets: Array<string> }>
 type DuplicateNewSecretsError = ApiError<
   'duplicate_new_secrets',
-  { duplicateSecrets: Array<SecretKey> }
+  { duplicateSecrets: Array<string> }
 >
