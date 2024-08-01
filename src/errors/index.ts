c1cc605b8f7f47afb2ef2a54c118bde93f0b5daa
dimak <rahbussiness@gmail.com>
2024-08-01 12:33:11 +0200

    feat: add function "createApiError", api errors

---
+++ src/errors/index.ts
@@ -0,0 +1,29 @@
+import { ApiError, ApiErrorDetails } from '../http/response'
+
+export const createApiError = <T extends string, D = undefined | ApiErrorDetails>(args: {
+  code: T
+  message: string
+  details: D
+}) => {
+  const error: ApiError<T, D> = {
+    code: args.code,
+    message: args.message,
+    details: args.details,
+  }
+
+  return error
+}
+
+export const invalidEnvironmentNameError = createApiError({
+  code: 'invalid_environment_name',
+  details: undefined,
+  message:
+    'Project name must be alphanumeric, only hyphens and underscores allowed, min 2 and max 255 characters.',
+})
+
+export const invalidProjectNameError = createApiError({
+  code: 'invalid_project_name',
+  details: undefined,
+  message:
+    'Project name must be alphanumeric, only hyphens and underscores allowed, min 2 and max 255 characters.',
+})
