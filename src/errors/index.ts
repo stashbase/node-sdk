d87459567c10d7346fe28b849d92fda172576f49
Radim Höfer <radim@stashbase.dev>
2024-08-01 12:41:28 +0200

    feat: change invalid env name erorr message

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -18,7 +18,7 @@ export const invalidEnvironmentNameError = createApiError({
   code: 'invalid_environment_name',
   details: undefined,
   message:
-    'Project name must be alphanumeric, only hyphens and underscores allowed, min 2 and max 255 characters.',
+    'Environment name must be alphanumeric, only underscores or hyphen separator allowed, min 2 and max 255 characters.',
 })
 
 export const invalidProjectNameError = createApiError({
