0fac392ac190ff2c02413a458bb2ec5d0d802ea4
Radim Höfer <radim.engineering@stashbase.dev>
2024-08-07 20:47:50 +0200

    feat: add prefixes to error codes to function constructors

--- src/errors/secrets.ts
+++ src/errors/secrets.ts
@@ -1,15 +1,15 @@
 import { ApiError } from '../http/response'
 import {
-  DuplicateNewSecretKeysError,
-  DuplicateSecretsKeysError,
-  InvalidNewSecretKeysError,
-  InvalidSecretKeyError,
-  InvalidSecretKeysError,
-  MissingPropertiesToUpdateError,
-  NewSecretKeysSameAsKeysError,
-  NoDataProvided,
+  DuplicateNewSecretKeysValidationError,
+  DuplicateSecretsKeysValidationError,
+  InvalidNewSecretKeysValidationError,
+  InvalidSecretKeyValidationError,
+  InvalidSecretKeysValidationError,
+  MissingPropertiesToUpdateValidationError,
+  NewSecretKeysSameAsKeysValidationError,
+  NoDataProvidedValidationError,
   SecretsErrorDetails,
-  SelfReferencingSecretsError,
+  SelfReferencingSecretsValidationError,
 } from '../types/errors/secrets'
 
 export const createSecretsError = <T extends string, D = undefined | SecretsErrorDetails>(args: {
@@ -29,25 +29,29 @@ export const createSecretsError = <T extends string, D = undefined | SecretsErro
 export const secretKeyFormatErrorMessage =
   'Secret keys cannot start with a digit, only uppercase alphanumeric characters and underscores allowed, min 2 and max 255 characters.'
 
-export const invalidSecretKeysError = (secretKeys: Array<string>): InvalidSecretKeysError =>
+export const invalidSecretKeysError = (
+  secretKeys: Array<string>
+): InvalidSecretKeysValidationError =>
   createSecretsError({
-    code: 'invalid_secret_keys',
+    code: 'validation.invalid_secret_keys',
     message: secretKeyFormatErrorMessage,
     details: {
       secretKeys,
     },
   })
 
-export const invalidSecretKeyError = (): InvalidSecretKeyError =>
+export const invalidSecretKeyError = (): InvalidSecretKeyValidationError =>
   createSecretsError({
-    code: 'invalid_secret_key',
+    code: 'validation.invalid_secret_key',
     message: secretKeyFormatErrorMessage,
     details: undefined,
   })
 
-export const duplicateSecretKeysError = (secretKeys: Array<string>): DuplicateSecretsKeysError =>
+export const duplicateSecretKeysError = (
+  secretKeys: Array<string>
+): DuplicateSecretsKeysValidationError =>
   createSecretsError({
-    code: 'duplicate_secret_keys',
+    code: 'validation.duplicate_secret_keys',
     message: `One or more secrets with the same value of property 'key' provided in the request.`,
     details: {
       secretKeys,
@@ -56,9 +60,9 @@ export const duplicateSecretKeysError = (secretKeys: Array<string>): DuplicateSe
 
 export const duplicateNewSecretKeysError = (
   secretKeys: Array<string>
-): DuplicateNewSecretKeysError =>
+): DuplicateNewSecretKeysValidationError =>
   createSecretsError({
-    code: 'duplicate_new_secret_keys',
+    code: 'validation.duplicate_new_secret_keys',
     message: `One or more secrets with the same value of property 'newKey' provided in the request.`,
     details: {
       secretKeys,
@@ -67,9 +71,9 @@ export const duplicateNewSecretKeysError = (
 
 export const selfReferencingSecretsError = (
   secretKeys: Array<string>
-): SelfReferencingSecretsError =>
+): SelfReferencingSecretsValidationError =>
   createSecretsError({
-    code: 'self_referencing_secrets',
+    code: 'validation.self_referencing_secrets',
     message: 'Self referencing secrets are not allowed.',
     details: {
       secretKeys,
@@ -78,27 +82,31 @@ export const selfReferencingSecretsError = (
 
 export const missingPropertiesToUpdateError = (
   secretKeys: Array<string>
-): MissingPropertiesToUpdateError =>
+): MissingPropertiesToUpdateValidationError =>
   createSecretsError({
-    code: 'missing_properties_to_update',
+    code: 'validation.missing_properties_to_update',
     message: 'At least one property to update must be provided.',
     details: {
       secretKeys,
     },
   })
 
-export const invalidNewSecretKeysError = (secretKeys: Array<string>): InvalidNewSecretKeysError =>
+export const invalidNewSecretKeysError = (
+  secretKeys: Array<string>
+): InvalidNewSecretKeysValidationError =>
   createSecretsError({
-    code: 'invalid_new_secret_keys',
+    code: 'validation.invalid_new_secret_keys',
     message: secretKeyFormatErrorMessage,
     details: {
       secretKeys,
     },
   })
 
-export const newKeysSameAsKeys = (secretKeys: Array<string>): NewSecretKeysSameAsKeysError =>
+export const newKeysSameAsKeys = (
+  secretKeys: Array<string>
+): NewSecretKeysSameAsKeysValidationError =>
   createSecretsError({
-    code: 'new_secret_keys_same_as_keys',
+    code: 'validation.new_secret_keys_same_as_keys',
     message:
       "One or more values of property 'newKey' match the provided 'key' values in the request.",
     details: {
@@ -106,9 +114,9 @@ export const newKeysSameAsKeys = (secretKeys: Array<string>): NewSecretKeysSameA
     },
   })
 
-export const noDataProvidedError = (): NoDataProvided =>
+export const noDataProvidedError = (): NoDataProvidedValidationError =>
   createSecretsError({
     details: undefined,
-    code: 'no_data_provided',
+    code: 'validation.no_data_provided',
     message: 'At least one data item must be provided.',
   })
