82060acce7f6fa115b6b47d4583e98e672628db8
Radim Höfer <radim.engineering@stashbase.dev>
2024-08-07 20:46:44 +0200

    feat: use prefixed errors for environment errors

--- src/types/errors/environments.ts
+++ src/types/errors/environments.ts
@@ -1,6 +1,12 @@
-import { ApiError } from '../../http/response'
+import { ConflictApiError, QuotaLimitApiError, ResourceApiError } from '.'
 
-export type EnvironmentNotFoundError = ApiError<'environment_not_found', undefined>
-export type EnvironmentAlreadyExistsError = ApiError<'environment_already_exists', undefined>
-export type EnvironmentLockedError = ApiError<'environment_locked', undefined>
-export type EnvironmentLimitReachedError = ApiError<'environment_limit_reached', undefined>
+export type EnvironmentNotFoundError = ResourceApiError<'environment_not_found', undefined>
+export type EnvironmentAlreadyExistsError = ConflictApiError<
+  'environment_already_exists',
+  undefined
+>
+export type EnvironmentLockedError = ConflictApiError<'environment_locked', undefined>
+export type EnvironmentLimitReachedError = QuotaLimitApiError<
+  'environment_limit_reached',
+  undefined
+>
