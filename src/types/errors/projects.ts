c6b75a22733df9fc7f338348eeed73b8159f8429
Radim Höfer <radim@stashbase.dev>
2024-08-11 17:28:17 +0200

    feat: add id name format validation api error type

--- src/types/errors/projects.ts
+++ src/types/errors/projects.ts
@@ -1,5 +1,14 @@
-import { ConflictApiError, ResourceApiError } from '.'
+import { ConflictApiError, ResourceApiError, ValidationApiError } from '.'
 
 export type ProjectNotFoundError = ResourceApiError<'project_not_found', undefined>
 export type ProjectLimitReachedError = ResourceApiError<'project_limit_reached', undefined>
 export type ProjectAlreadyExistsError = ConflictApiError<'project_already_exists', undefined>
+export type ProjectCannotUseIdFormatNameError = ValidationApiError<
+  'project_name_cannot_use_id_format',
+  {
+    example: {
+      validProjectNames: string[]
+      invalidProjectNames: string[]
+    }
+  }
+>
