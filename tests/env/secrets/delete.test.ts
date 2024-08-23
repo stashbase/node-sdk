f08040660750cb88e254a931c1a9f2f0a9845f4c
Radim Höfer <radim@stashbase.dev>
2024-08-23 15:17:55 +0200

    chore: use env variable api key for env tests

--- tests/env/secrets/delete.test.ts
+++ tests/env/secrets/delete.test.ts
@@ -2,7 +2,7 @@ import { test } from 'vitest'
 import { createEnvApi } from '../../../src'
 
 test('Delete specific secret from env - grant must be write or read/write', async () => {
-  const envApi = createEnvApi('-YSbVSq5kzmc1EgzAO9SThUXFQLjb6R1')
+  const envApi = createEnvApi(process.env.VITE_TEST_ENV_API_KEY as string)
 
   const { data, error } = await envApi.secrets.remove(['NEXT_PUBLIC_PRODUCTION_GITHUB_URL'])
   if (error) {
