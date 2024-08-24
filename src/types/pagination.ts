e61a1fa543b62bd3a37c5cf4ccbac1ae94ed8968
Radim Höfer <radim@stashbase.dev>
2024-08-24 15:26:38 +0200

    refactor: add js doc comments to types

--- src/types/pagination.ts
+++ src/types/pagination.ts
@@ -1,8 +1,17 @@
+/**
+ * Interface representing metadata for pagination.
+ */
 export interface PaginationMetadata {
+  /** The current page number. */
   page: number
+  /** The maximum number of items per page. */
   limit: number
+  /** The total number of items across all pages. */
   totalItems: number
+  /** The total number of pages. */
   totalPages: number
+  /** The number of the next page, or null if there is no next page. */
   nextPage: number | null
+  /** The number of the previous page, or null if there is no previous page. */
   prevPage: number | null
 }
