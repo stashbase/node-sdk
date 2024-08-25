3cf5edcc850c4a7ab9057305bf1e6805402cb19a
Radim Höfer <radim.engineering@stashbase.dev>
2024-08-25 14:50:41 +0200

    chore: update test for duplicate environment

--- tests/workspace/environments/duplicate.test.ts
+++ tests/workspace/environments/duplicate.test.ts
@@ -5,9 +5,9 @@ describe('Duplicate environment', () => {
   test('', async () => {
     const stashbase = createEnvEase(process.env.VITE_TEST_WORKSPACE_API_KEY as string)
 
-    const { error } = await onestash.environments.duplicate({
+    const { error } = await stashbase.environments.duplicate({
       project: 'hero-hub',
-      name: 'aws',
+      environment: 'aws',
       duplicateName: 'aws_copy',
     })
 
