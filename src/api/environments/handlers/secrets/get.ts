14053c2c846758980e5f1fe7753697518f793ead
Radim Höfer <radim@stashbase.dev>
2024-08-27 11:50:42 +0200

    refactor: get secret for env client - use list req function

--- src/api/environments/handlers/secrets/get.ts
+++ src/api/environments/handlers/secrets/get.ts
@@ -1,44 +1,23 @@
 import { HttpClient } from '../../../../http/client'
-import { createApiErrorFromResponse } from '../../../../errors'
-import { GetSecretError } from '../../../../types/errors/secrets'
 import { ApiResponse, responseFailure, responseSuccess } from '../../../../http/response'
-import { GetSecretOptions, GetSecretQueryParams, GetSecretResData } from '../../../../types/secrets'
+import { GetSecretOptions, Secret } from '../../../../types/secrets'
+import { listSecrets } from './list'
+import { SecretKey } from '../../../../types/secretKey'
+import { GenericApiError } from '../../../../types/errors'
 
 async function getSecret(
   envClient: HttpClient,
-  key: string,
+  key: SecretKey,
   options?: GetSecretOptions
-): Promise<ApiResponse<GetSecretResData, GetSecretError>> {
-  const omit = options?.omit
-  const expandRefs = options?.expandRefs
+): Promise<ApiResponse<Secret | null, GenericApiError>> {
+  const { data, error } = await listSecrets({ envClient, options, only: [key] })
 
-  const query: GetSecretQueryParams = {}
-
-  if (expandRefs) {
-    if (!omit?.includes('value')) {
-      query['expand-refs'] = true
-    }
+  if (error) {
+    return responseFailure(error)
   }
 
-  if (omit && omit.length > 0) {
-    const omitStr = omit.filter((o) => o === 'value' || o === 'description').join(',')
-
-    if (omitStr.length > 0) {
-      query['omit'] = omitStr
-    }
-  }
-
-  try {
-    const secrets = await envClient.get<GetSecretResData>({
-      path: `/v1/secrets/${key}`,
-      query: Object.keys(query).length > 0 ? query : undefined,
-    })
-
-    return responseSuccess(secrets)
-  } catch (error) {
-    const apiError = createApiErrorFromResponse<GetSecretError>(error)
-    return responseFailure(apiError)
-  }
+  const secret = data?.[0] ?? null
+  return responseSuccess(secret)
 }
 
 export { getSecret }
