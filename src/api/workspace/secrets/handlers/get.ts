9da2f371890f5f43f0e3b0111688f5da089cde7a
Radim Höfer <radim@stashbase.dev>
2024-08-27 11:51:18 +0200

    refactor: get secret for workspace client - use list req function

--- src/api/workspace/secrets/handlers/get.ts
+++ src/api/workspace/secrets/handlers/get.ts
@@ -1,13 +1,13 @@
 import { HttpClient } from '../../../../http/client'
 import { SecretKey } from '../../../../types/secretKey'
-import { createApiErrorFromResponse } from '../../../../errors'
 import { ApiResponse, responseFailure, responseSuccess } from '../../../../http/response'
 import { EnvironmentNotFoundError, ProjectNotFoundError } from '../../../../types/errors'
 import { GetSecretError as SharedGetSecretsError } from '../../../../types/errors/secrets'
-import { GetSecretOptions, GetSecretQueryParams, GetSecretResData } from '../../../../types/secrets'
+import { GetSecretOptions, Secret } from '../../../../types/secrets'
+import { listSecrets } from './list'
 
 type GetSecretError = SharedGetSecretsError | ProjectNotFoundError | EnvironmentNotFoundError
-type GetSecretResponse = Promise<ApiResponse<GetSecretResData, GetSecretError>>
+type GetSecretResponse = Promise<ApiResponse<Secret | null, GetSecretError>>
 
 export type GetSecretArgs = {
   project: string
@@ -18,36 +18,17 @@ export type GetSecretArgs = {
 } & GetSecretOptions
 
 async function getSecret(envClient: HttpClient, args: GetSecretArgs): GetSecretResponse {
-  const { project, environment, key } = args
-  const { expandRefs, omit } = args
+  const { data, error } = await listSecrets(envClient, {
+    ...args,
+    only: [args.key],
+  })
 
-  const query: GetSecretQueryParams = {}
-
-  if (expandRefs) {
-    if (!omit?.includes('value')) {
-      query['expand-refs'] = true
-    }
+  if (error) {
+    return responseFailure(error)
   }
 
-  if (omit && omit.length > 0) {
-    const omitStr = omit.filter((o) => o === 'value' || o === 'description').join(',')
-
-    if (omitStr.length > 0) {
-      query['omit'] = omitStr
-    }
-  }
-
-  try {
-    const secret = await envClient.get<GetSecretResData>({
-      path: `/v1/projects/${project}/environments/${environment}/secrets/${key}`,
-      query: Object.keys(query).length > 0 ? query : undefined,
-    })
-
-    return responseSuccess(secret)
-  } catch (error) {
-    const apiError = createApiErrorFromResponse<GetSecretError>(error)
-    return responseFailure(apiError)
-  }
+  const secret = data?.[0] ?? null
+  return responseSuccess(secret)
 }
 
 export { getSecret }
