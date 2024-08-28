7ed9dbd9248807b3668ad0674a2d5cc8125df6b3
Radim Höfer <radim.engineering@stashbase.dev>
2024-08-28 15:27:05 +0200

    refactor: add, improve webhook error types

--- src/types/errors/webhooks.ts
+++ src/types/errors/webhooks.ts
@@ -1,14 +1,57 @@
-import { GenericApiError, ResourceApiError } from '.'
+import { ConflictApiError, GenericApiError, ResourceApiError, ValidationApiError } from '.'
 
 export type ListWebhooksError = GenericApiError
 export type CreateWebhookError = GenericApiError
 
-export type UpdateWebhookError = GenericApiError | WebhookNotFoundError
-export type DeleteWebhookError = GenericApiError | WebhookNotFoundError
-export type GetWebhookError = GenericApiError | WebhookNotFoundError
-export type RotateWebhookSigningSecretError = GenericApiError | WebhookNotFoundError
-export type GetWebhookSigningSecretError = GenericApiError | WebhookNotFoundError
-export type TestWebhookError = GenericApiError | WebhookNotFoundError
-export type ListWebhookLogsError = GenericApiError | WebhookNotFoundError
+export type UpdateWebhookError =
+  | SingleWebhookError
+  | WebhookMissingPropertiesToUpdateValidationError
+
+export type UpdateWebhookStatusError =
+  | SingleWebhookError
+  | WebhookAlreadyEnabledConflictError
+  | WebhookAlreadyDisabledConflictError
+
+export type DeleteWebhookError = SingleWebhookError
+export type GetWebhookError = SingleWebhookError
+
+export type RotateWebhookSigningSecretError = SingleWebhookError
+export type GetWebhookSigningSecretError = SingleWebhookError
+export type TestWebhookError = SingleWebhookError
+export type ListWebhookLogsError = SingleWebhookError
+
+//
+export type SingleWebhookError = GenericApiError | InvalidWebhookIdError | WebhookNotFoundError
+
+export type InvalidWebhookIdError = ValidationApiError<
+  'invalid_webhook_id',
+  {
+    exampleWebhookId: string
+  }
+>
+
+export type WebhookUrlHttpsRequiredValidationError = ValidationApiError<
+  'webhook_url_https_required',
+  {
+    validUrlExample: string
+  }
+>
+
+export type WebhookMissingPropertiesToUpdateValidationError = ValidationApiError<
+  'missing_properties_to_update',
+  {
+    possibleProperties: Array<'url' | 'description'>
+  }
+>
+
+export type WebhookAlreadyEnabledConflictError = ConflictApiError<
+  'webhook_already_enabled',
+  undefined
+>
+
+export type WebhookAlreadyDisabledConflictError = ConflictApiError<
+  'webhook_already_disabled',
+  undefined
+>
 
 type WebhookNotFoundError = ResourceApiError<'webhook_not_found', undefined>
