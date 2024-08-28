497a064b0779137975056b1e034a1b70a2f0ef70
Radim Höfer <radim@stashbase.dev>
2024-08-28 14:07:49 +0200

    feat: implement delete webhook - env api

---
+++ src/api/environments/handlers/webhooks/delete.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '../../../../http/client'
+import { createApiErrorFromResponse } from '../../../../errors'
+import { GetWebhookError } from '../../../../types/errors/webhooks'
+import { ApiResponse, responseFailure, responseSuccess } from '../../../../http/response'
+
+async function deleteWebhook(
+  envClient: HttpClient,
+  webhookId: string
+): Promise<ApiResponse<null, GetWebhookError>> {
+  try {
+    const webhook = await envClient.del<null>({
+      path: `/v1/webhooks/${webhookId}`,
+    })
+
+    return responseSuccess(webhook)
+  } catch (error) {
+    const apiError = createApiErrorFromResponse<GetWebhookError>(error)
+    return responseFailure(apiError)
+  }
+}
+
+export { deleteWebhook }
