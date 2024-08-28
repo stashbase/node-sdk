6a1ba4cf40b27a4ba4b86f3628f6403df4b95d36
Radim Höfer <radim.engineering@stashbase.dev>
2024-08-28 21:10:34 +0200

    feat: implement update webhook status - workspace api

---
+++ tests/workspace/webhooks/updateStatus.test.ts
@@ -0,0 +1,20 @@
+import { test } from 'vitest'
+import { createWorkspaceClient } from '../../../src'
+
+test('Disable webhook', async () => {
+  const stashbase = createWorkspaceClient(process.env.VITE_TEST_WORKSPACE_API_KEY as string)
+
+  const { data, error } = await stashbase.webhooks.disable({
+    project: 'name',
+    environment: '123',
+    webhookId: 'wh_4i1gbnewYBnCTZg3Sbye2c',
+  })
+
+  if (error) {
+    const { code } = error
+    console.log(code)
+  } else {
+    console.log('Data:\n')
+    console.log(data)
+  }
+})
