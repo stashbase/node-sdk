47171993a9d7834b3c17c31051b4820a0ef35d7c
Radim Höfer <radim.engineering@stashbase.dev>
2024-08-28 21:08:51 +0200

    feat: implement list webhook logs - workspace api

--- src/api/workspace/webhooks/api.ts
+++ src/api/workspace/webhooks/api.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '../../../http/client'
 import { listWebhooks, ListWebhooksArgs } from './handlers/list'
+import { listWebhookLogs, ListWebhookLogsArgs } from './handlers/listLogs'
 export class WebhooksAPI {
   private httpClient: HttpClient
 
@@ -17,5 +18,18 @@ export class WebhooksAPI {
    */
   async list(args: ListWebhooksArgs) {
     return await listWebhooks(this.httpClient, args)
+  }
+
+  /**
+   * Lists logs for a specific webhook.
+   *
+   * @param args - The arguments for listing webhook logs.
+   * @param args.project - The name or id of the project.
+   * @param args.environment - The name or id of the environment.
+   * @param args.webhookId - The id of the webhook.
+   * @returns A promise that resolves to an array of webhook log objects with pagination metadata or an error response.
+   */
+  async listLogs(args: ListWebhookLogsArgs) {
+    return await listWebhookLogs(this.httpClient, args)
   }
 }
---
+++ tests/workspace/webhooks/listLogs.test.ts
@@ -0,0 +1,20 @@
+import { test } from 'vitest'
+import { createWorkspaceClient } from '../../../src'
+
+test('List webhook logs', async () => {
+  const stashbase = createWorkspaceClient(process.env.VITE_TEST_WORKSPACE_API_KEY as string)
+
+  const { data, error } = await stashbase.webhooks.listLogs({
+    project: 'name',
+    environment: '123',
+    webhookId: 'wh_4i1gbnewYBnCTZg3Sbye2c',
+  })
+
+  if (error) {
+    const { code } = error
+    console.log(code)
+  } else {
+    console.log('Data:\n')
+    console.log(data)
+  }
+})
