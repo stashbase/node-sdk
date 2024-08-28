0ee9267b408d3c51f7577cd5c23cfdbca8fbc00b
dimak <rahbussiness@gmail.com>
2024-08-28 10:31:06 +0200

    refactor: rename workspace api secrets methods "remove", "removeAll" to "delete", "deleteAll"

--- tests/workspace/secrets/delete.test.ts
+++ tests/workspace/secrets/delete.test.ts
@@ -4,7 +4,7 @@ import { createWorkspaceClient } from '../../../src'
 test('Delete secrets', async () => {
   const envEase = createWorkspaceClient(process.env.WORKSPACE_API_KEY as string)
 
-  const { data, error } = await envEase.secrets.remove({
+  const { data, error } = await envEase.secrets.delete({
     project: 'pr_iBgCx5tegfVaKzjywTg2ck',
     environment: 'ev_3NCdY7kmbLJpZcpMg4W6wk',
     keys: ['KEYS', 'SECRET_234'],
