e6c404d0c77393c79a2a19688995c26bfe80604e
Radim Höfer <radim.engineering@stashbase.dev>
2024-08-28 10:19:11 +0200

    chore: remove function args from test

--- tests/env/secrets/list.test.ts
+++ tests/env/secrets/list.test.ts
@@ -4,7 +4,7 @@ import { createEnvClient } from '../../../src'
 test('List environment secrets', async () => {
   const envApi = createEnvClient(process.env.VITE_TEST_ENV_API_KEY as string)
 
-  const { data, error } = await envApi.secrets.list({ description: true })
+  const { data, error } = await envApi.secrets.list()
   if (error) {
     const { code } = error
     console.log(code)
