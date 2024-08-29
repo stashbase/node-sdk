307248bd5655de13d480d243c1dac7f2b705f1c0
Radim Höfer <radim.engineering@stashbase.dev>
2024-08-29 20:38:43 +0200

    feat: add reusable argument types for webhooks

--- src/types/aruguments.ts
+++ src/types/aruguments.ts
@@ -5,3 +5,13 @@ export type ProjectEnvHandlerArgs<T extends Record<string, unknown> | undefined>
         environment: string
       }
     : { project: string; environment: string } & T
+
+export type SingleWebhookProjectEnvHandlerArgs<T extends Record<string, unknown> | undefined> = {
+  /** The ID of the webhook */
+  webhookId: string
+} & ProjectEnvHandlerArgs<T>
+
+export type SingleWebhookArgs<T extends Record<string, unknown> | undefined> = {
+  /** The ID of the webhook */
+  webhookId: string
+} & T
