9776e447eff4be1d57114780dce511fbb4a7a57e
Radim Höfer <radim.engineering@stashbase.dev>
2024-08-29 15:10:53 +0200

    feat: add invalid webhook url validation error

--- src/errors/webhooks.ts
+++ src/errors/webhooks.ts
@@ -3,6 +3,7 @@ import {
   InvalidWebhookIdError,
   InvalidWebhookLogsLimitError,
   InvalidWebhookLogsPageError,
+  InvalidWebhookUrlValidationError,
   WebhookMissingPropertiesToUpdateValidationError,
   WebhookUrlHttpsRequiredValidationError,
 } from '../types/errors/webhooks'
@@ -13,6 +14,14 @@ export const invalidWebhookIdError: InvalidWebhookIdError = createApiError({
     "Provided webhook Id is is not valid. Webhook Ids have prefix 'wh_' and must be 25 characters long.",
   details: {
     exampleWebhookId: 'wh_m1DAScGeaJfFLSFUzTjiq8',
+  },
+})
+
+export const invalidWebhookUrlError: InvalidWebhookUrlValidationError = createApiError({
+  code: 'validation.invalid_webhook_url',
+  message: `The webhook URL must be valid and use HTTPS for secure communication. Please provide a URL that starts with 'https://'.`,
+  details: {
+    validUrlExample: 'https://my-endpoint.com',
   },
 })
 
