484fd0c27bafecdf00a6d04b1431c3234a566977
Radim Höfer <radim.engineering@stashbase.dev>
2024-08-30 12:48:58 +0200

    refactor: separate type CreateWebhookData

--- src/api/environments/handlers/webhooks/create.ts
+++ src/api/environments/handlers/webhooks/create.ts
@@ -1,17 +1,8 @@
 import { HttpClient } from '../../../../http/client'
 import { createApiErrorFromResponse } from '../../../../errors'
-import { CreateWebhookResponse } from '../../../../types/webhooks'
+import { CreateWebhookData, CreateWebhookResponse } from '../../../../types/webhooks'
 import { CreateWebhookError } from '../../../../types/errors/webhooks'
 import { ApiResponse, responseFailure, responseSuccess } from '../../../../http/response'
-
-export interface CreateWebhookData {
-  /** The target URL of the webhook (must use HTTPS) */
-  url: string
-  /** Whether the webhook is enabled  or not (default: false) */
-  enabled?: boolean
-  /** The description of the webhook */
-  description?: string | null
-}
 
 async function createWebhook(
   envClient: HttpClient,
