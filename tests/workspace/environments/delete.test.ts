2e5c1055588459c48bd9b6ed6648496871c8ddeb
Radim Höfer <radim@stashbase.dev>
2024-08-31 09:10:58 +0200

    chore: update tests for workspace environments

--- tests/workspace/environments/delete.test.ts
+++ tests/workspace/environments/delete.test.ts
@@ -3,26 +3,19 @@ import { createWorkspaceClient } from '../../../src'
 
 describe('Delete environment', () => {
   test('Invalid token', async () => {
-    const envEase = createWorkspaceClient('4r4')
+    const envEase = createWorkspaceClient(process.env.VITE_TEST_WORKSPACE_API_KEY as string)
 
-    const { data, error } = await envEase.environments.delete({
-      project: 'hero-hub1231231231',
-      environment: 'dev-sdk',
-    })
+    const { data, error } = await envEase
+      .environments('pr_nVpZPsL5nQTUP9yXU2GKYJ')
+      .delete('app-dev')
 
     console.log(data)
     console.log(error)
-
-    assert.equal(error?.code, 'invalid_token')
   })
 
   test('Project not found', async () => {
     const envEase = createWorkspaceClient('xPKDa2Xq0zWmfES1nLDoG45qZtR1z2qL')
-
-    const { data, error } = await envEase.environments.delete({
-      project: 'hero-hub1231231231',
-      environment: 'dev-sdk',
-    })
+    const { data, error } = await envEase.environments('hero-hub1231231231').delete('dev-sdk')
 
     assert.equal(error?.code, 'project_not_found')
 
