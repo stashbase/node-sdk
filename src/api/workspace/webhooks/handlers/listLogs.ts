244b63ef6316a1800893209558a98061e5f17933
Radim Höfer <radim.engineering@stashbase.dev>
2024-08-31 09:10:22 +0200

    fix: use list webhook logs options query

--- src/api/workspace/webhooks/handlers/listLogs.ts
+++ src/api/workspace/webhooks/handlers/listLogs.ts
@@ -22,9 +22,20 @@ async function listWebhookLogs(
 ): Promise<ApiResponse<ListWebhookLogsResponse, ListWebhookLogsError>> {
   const { client, webhookId, project, environment, opts } = args
 
+  const query: Record<string, string | number> = {}
+
+  if (opts?.page) {
+    query.page = opts.page
+  }
+
+  if (opts?.limit) {
+    query.limit = opts.limit
+  }
+
   try {
     const webhookLogs = await client.get<ListWebhookLogsResponse>({
       path: `/v1/projects/${project}/environments/${environment}/webhooks/${webhookId}/logs`,
+      query,
     })
 
     return responseSuccess(webhookLogs)
