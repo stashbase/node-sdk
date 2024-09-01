0e01503c5b16cc12c6ee02d57873586dea31c7ba
Radim Höfer <radim@stashbase.dev>
2024-09-01 15:39:12 +0200

    feat: revert changelog change returns id

--- src/api/workspace/changelog/handlers/revert.ts
+++ src/api/workspace/changelog/handlers/revert.ts
@@ -1,6 +1,7 @@
 import { createApiErrorFromResponse } from '../../../../errors'
 import { ApiResponse, responseFailure, responseSuccess } from '../../../../http/response'
 import { ProjectEnvHandlerArgs } from '../../../../types/aruguments'
+import { RevertChangeResponse } from '../../../../types/changelog'
 import { EnvironmentNotFoundError, ProjectNotFoundError } from '../../../../types/errors'
 import { RevertChangelogChangeError as SharedRevertChangelogChangeError } from '../../../../types/errors/changelog'
 
@@ -15,11 +16,11 @@ type RevertChangelogChangeArgs = ProjectEnvHandlerArgs<{
 
 async function revertChangelogChange(
   args: RevertChangelogChangeArgs
-): Promise<ApiResponse<null, RevertChangelogChangeError>> {
+): Promise<ApiResponse<RevertChangeResponse, RevertChangelogChangeError>> {
   const { client, project, environment, changeId } = args
 
   try {
-    const changelog = await client.post<null>({
+    const changelog = await client.post<RevertChangeResponse>({
       path: `/v1/projects/${project}/environments/${environment}/changelog/${changeId}/revert`,
     })
 
