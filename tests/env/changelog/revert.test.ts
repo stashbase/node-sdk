c14b1ea28c1739c0c622416ec12b32405f7caf14
Radim Höfer <radim.engineering@stashbase.dev>
2024-09-01 13:59:37 +0200

    chore: add tests for env api changelog

---
+++ tests/env/changelog/revert.test.ts
@@ -0,0 +1,15 @@
+import { test } from 'vitest'
+import { createEnvClient } from '../../../src'
+
+test('Revert change', async () => {
+  const stashbase = createEnvClient(process.env.VITE_TEST_ENV_API_KEY as string)
+
+  const { data, error } = await stashbase.changelog.revert('ch_6pfHNDhXLNMZkgpmhA61cY')
+
+  if (error) {
+    const { code } = error
+    console.log(code)
+  } else {
+    console.log(data)
+  }
+})
