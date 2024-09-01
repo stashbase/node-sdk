b0370073372ec436f5c26ea5690129bc2b742406
Radim Höfer <radim.engineering@stashbase.dev>
2024-09-01 14:01:54 +0200

    feat: implement list changelog for workspace api

--- src/api/workspace/changelog/api.ts
+++ src/api/workspace/changelog/api.ts
@@ -1,10 +1,20 @@
 import { invalidEnvironmentIdentifierError, invalidProjectIdentifierError } from '../../../errors'
+import {
+  invalidChangelogChangeIdError,
+  invalidChangelogLimitError,
+  invalidChangelogPageError,
+} from '../../../errors/changelog'
 import { HttpClient } from '../../../http/client'
+import { ApiResponse, responseFailure } from '../../../http/response'
+import { ListChangelogOptions, ListChangelogResponse } from '../../../types/changelog'
+import { InvalidEnvironmentIdentifierError } from '../../../types/errors/environments'
+import { InvalidIdentifierProjectError } from '../../../types/errors/projects'
 import {
   isValidChangelogChangeId,
   isValidEnvironmentIdentifier,
   isValidProjectIdentifier,
 } from '../../../utils/inputValidation'
+import { listChangelog, ListChangelogError } from './handlers/list'
 export class ChangelogAPI {
   private httpClient: HttpClient
   public project: string
@@ -36,5 +46,44 @@ export class ChangelogAPI {
       const error = invalidChangelogChangeIdError
       return error
     }
+  }
+
+  /**
+   * Lists the changelog items.
+   *
+   * @param withValues - Whether to include values in the changelog items.
+   * @param options - Options for listing the changelog items.
+   * @returns A promise that resolves to the list of changelog items or an error response.
+   */
+  public async list<T extends boolean | undefined = undefined>(
+    withValues?: T,
+    options?: ListChangelogOptions
+  ): Promise<ApiResponse<ListChangelogResponse<T>, ListChangelogError>> {
+    const validationError = this.validateIdentifiers()
+
+    if (validationError)
+      return responseFailure(
+        validationError as InvalidIdentifierProjectError | InvalidEnvironmentIdentifierError
+      )
+
+    if (options) {
+      if (
+        (options.page !== undefined && (options.page <= 0 || options.page > 1000)) ||
+        typeof options.page !== 'number'
+      ) {
+        const error = invalidChangelogPageError
+        return responseFailure(error)
+      }
+
+      if (
+        (options.limit !== undefined && (options.limit < 2 || options.limit > 10)) ||
+        typeof options.limit !== 'number'
+      ) {
+        const error = invalidChangelogLimitError
+        return responseFailure(error)
+      }
+    }
+
+    return listChangelog({ ...this.getHandlerArgs(), client: this.httpClient, withValues, options })
   }
 }
---
+++ src/api/workspace/changelog/handlers/list.ts
@@ -0,0 +1,54 @@
+import { createApiErrorFromResponse } from '../../../../errors'
+import { ApiResponse, responseFailure, responseSuccess } from '../../../../http/response'
+import { ProjectEnvHandlerArgs } from '../../../../types/aruguments'
+import { ListChangelogOptions, ListChangelogResponse } from '../../../../types/changelog'
+import { EnvironmentNotFoundError, ProjectNotFoundError } from '../../../../types/errors'
+import { ListChangelogError as SharedListChangelogError } from '../../../../types/errors/changelog'
+import { InvalidEnvironmentIdentifierError } from '../../../../types/errors/environments'
+import { InvalidIdentifierProjectError } from '../../../../types/errors/projects'
+
+export type ListChangelogError =
+  | SharedListChangelogError
+  | InvalidIdentifierProjectError
+  | InvalidEnvironmentIdentifierError
+  | ProjectNotFoundError
+  | EnvironmentNotFoundError
+
+type ListChangelogArgs = ProjectEnvHandlerArgs<{
+  withValues?: boolean
+  options?: ListChangelogOptions
+}>
+
+async function listChangelog(
+  args: ListChangelogArgs
+): Promise<ApiResponse<ListChangelogResponse<typeof args.withValues>, ListChangelogError>> {
+  const { client, project, environment, options } = args
+
+  const query: Record<string, string | number> = {}
+
+  if (args.withValues) {
+    query['show-values'] = 'true'
+  }
+
+  if (options?.page) {
+    query.page = options.page
+  }
+
+  if (options?.limit) {
+    query.limit = options.limit
+  }
+
+  try {
+    const changelog = await client.get<ListChangelogResponse<typeof args.withValues>>({
+      path: `/v1/projects/${project}/environments/${environment}/changelog`,
+      query,
+    })
+
+    return responseSuccess(changelog)
+  } catch (error) {
+    const apiError = createApiErrorFromResponse<ListChangelogError>(error)
+    return responseFailure(apiError)
+  }
+}
+
+export { listChangelog }
