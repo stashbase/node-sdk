e75c37ef9aa31e89cfaa5dcfd150ceebb2d731fd
Radim Höfer <radim.engineering@stashbase.dev>
2024-10-22 15:35:10 +0200

    refactor: rename handler function argument "key" to "name"

--- src/api/environments/handlers/secrets/delete.ts
+++ src/api/environments/handlers/secrets/delete.ts
@@ -1,17 +1,17 @@
 import { HttpClient } from '../../../../http/client'
 import { DeleteSecretsError } from '../../../../types/errors/secrets'
-import { DeleteSecretsResData } from '../../../../types/secrets'
+import { DeleteSecretsResData, SecretName } from '../../../../types/secrets'
 import { ApiResponse } from '../../../../http/response'
 
 async function deleteEnvironmentSecrets(
   client: HttpClient,
-  keys: Uppercase<string>[]
+  names: SecretName[]
 ): Promise<ApiResponse<DeleteSecretsResData, DeleteSecretsError>> {
   const path = '/v1/secrets/delete'
 
   return client.sendApiRequest<DeleteSecretsResData, DeleteSecretsError>({
     method: 'POST',
-    data: keys,
+    data: names,
     path,
   })
 }
