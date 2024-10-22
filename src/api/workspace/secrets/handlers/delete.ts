2fdd3805266d677945b36b179ff10b54ff3f8f33
dimak <rahbussiness@gmail.com>
2024-10-22 15:28:14 +0200

    feat: use "SecretName" type for workspace API handlers

--- src/api/workspace/secrets/handlers/delete.ts
+++ src/api/workspace/secrets/handlers/delete.ts
@@ -1,26 +1,25 @@
 import { ApiResponse } from '../../../../http/response'
-import { DeleteSecretsResData } from '../../../../types/secrets'
+import { DeleteSecretsResData, SecretName } from '../../../../types/secrets'
 import { EnvironmentNotFoundError, ProjectNotFoundError } from '../../../../types/errors'
 import { DeleteSecretsError as SharedDeleteSecretsError } from '../../../../types/errors/secrets'
 import { ProjectEnvHandlerArgs } from '../../../../types/aruguments'
-import { SecretKey } from '../../../../types/secretKey'
 
 type DeleteSecretsError = ProjectNotFoundError | EnvironmentNotFoundError | SharedDeleteSecretsError
 
 export type DeleteSecretsArgs = ProjectEnvHandlerArgs<{
-  keys: Array<SecretKey>
+  names: Array<SecretName>
 }>
 
 async function deleteSecrets(
   args: DeleteSecretsArgs
 ): Promise<ApiResponse<DeleteSecretsResData, DeleteSecretsError>> {
-  const { client, project, environment, keys } = args
+  const { client, project, environment, names } = args
   const path = `/v1/projects/${project}/environments/${environment}/secrets/delete`
 
   return await client.sendApiRequest<DeleteSecretsResData, DeleteSecretsError>({
     method: 'POST',
     path,
-    data: keys,
+    data: names,
   })
 }
 
