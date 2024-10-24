068c7eab4bd1565994ef349e8de5cbe4b60552b3
Radim Höfer <radim@stashbase.dev>
2024-10-24 09:36:31 +0200

    test: update test function argument

--- tests/workspace/environments/load.test.ts
+++ tests/workspace/environments/load.test.ts
@@ -5,7 +5,7 @@ test('Should load environment', async () => {
   const envEase = createWorkspaceClient(process.env.VITE_TEST_WORKSPACE_API_KEY as string)
 
   const { error } = await envEase.environments('proj_nVpZPsL5nQTUP9yXU2GKYJ').load('123', {
-    print: 'key',
+    print: 'name',
     expandRefs: true,
   })
 
