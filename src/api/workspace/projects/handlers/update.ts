2805ba3261d97a0fce622a2714f0dd5bd3f6aaef
Radim Höfer <radim@stashbase.dev>
2025-07-29 12:51:50 +0200

    feat: implement update project functionality with validation for name and description

---
+++ src/api/workspace/projects/handlers/update.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '../../../../http/client'
+import { ApiResponse } from '../../../../http/response'
+import {
+  ProjectAlreadyExistsError,
+  ProjectLimitReachedError,
+} from '../../../../types/errors/projects'
+import { GenericApiError } from '../../../../types/errors'
+import { AtLeastOne } from '../../../../types/util'
+
+type UpdateSecretsError = GenericApiError | ProjectAlreadyExistsError | ProjectLimitReachedError
+
+export type UpdateProjectData = AtLeastOne<{
+  /** The new name of the project */
+  name: string
+  /** The new description of the project */
+  description: string | null
+}>
+
+export async function updateProject(
+  envClient: HttpClient,
+  projectNameOrId: string,
+  data: UpdateProjectData
+): Promise<ApiResponse<null, UpdateSecretsError>> {
+  return await envClient.sendApiRequest<null, UpdateSecretsError>({
+    method: 'PATCH',
+    path: `/v1/projects/${projectNameOrId}`,
+    data,
+  })
+}
