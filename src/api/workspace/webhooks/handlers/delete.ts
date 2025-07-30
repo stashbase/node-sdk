8ef2fef9506ea4f28708dd04f89427ac3acfb22a
Radim Höfer <radim@stashbase.dev>
2025-07-30 16:17:08 +0200

    refactor: update DeleteWebhookError type to include EnvironmentContextError

--- src/api/workspace/webhooks/handlers/delete.ts
+++ src/api/workspace/webhooks/handlers/delete.ts
@@ -1,14 +1,18 @@
 import { GetWebhookError } from '../../../../types/errors/webhooks'
 import { SingleWebhookProjectEnvHandlerArgs } from '../../../../types/aruguments'
 import { ApiResponse } from '../../../../http/response'
+import { EnvironmentContextError } from '../../../../types/errors'
 
 export type DeleteWebhookArgs = SingleWebhookProjectEnvHandlerArgs<undefined>
+export type DeleteWebhookError = GetWebhookError | EnvironmentContextError
 
-async function deleteWebhook(args: DeleteWebhookArgs): Promise<ApiResponse<null, GetWebhookError>> {
+async function deleteWebhook(
+  args: DeleteWebhookArgs
+): Promise<ApiResponse<null, DeleteWebhookError>> {
   const { client, project, environment, webhookId } = args
   const path = `/v1/projects/${project}/environments/${environment}/webhooks/${webhookId}`
 
-  return await client.sendApiRequest<null, GetWebhookError>({
+  return await client.sendApiRequest<null, DeleteWebhookError>({
     method: 'DELETE',
     path,
   })
