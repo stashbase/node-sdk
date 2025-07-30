087ee357828fe75754f3a751d441604ebb2c89fd
Radim Höfer <radim@stashbase.dev>
2025-07-30 10:27:58 +0200

    refactor: rename UpdateSecretsError to UpdateProjectError for clarity in project update handling

--- src/api/workspace/projects/handlers/update.ts
+++ src/api/workspace/projects/handlers/update.ts
@@ -7,7 +7,7 @@ import {
 import { GenericApiError } from '../../../../types/errors'
 import { AtLeastOne } from '../../../../types/util'
 
-type UpdateSecretsError = GenericApiError | ProjectAlreadyExistsError | ProjectLimitReachedError
+type UpdateProjectError = GenericApiError | ProjectAlreadyExistsError | ProjectLimitReachedError
 
 export type UpdateProjectData = AtLeastOne<{
   /** The new name of the project */
@@ -20,8 +20,8 @@ export async function updateProject(
   envClient: HttpClient,
   projectNameOrId: string,
   data: UpdateProjectData
-): Promise<ApiResponse<null, UpdateSecretsError>> {
-  return await envClient.sendApiRequest<null, UpdateSecretsError>({
+): Promise<ApiResponse<null, UpdateProjectError>> {
+  return await envClient.sendApiRequest<null, UpdateProjectError>({
     method: 'PATCH',
     path: `/v1/projects/${projectNameOrId}`,
     data,
