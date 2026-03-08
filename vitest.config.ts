cc8a60bb5801989eee5674b7367e6a587e3a0145
Radim Höfer <radim.engineering@stashbase.dev>
2026-03-08 08:26:40 +0100

    chore: ensure SDK version is defined in vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from 'vitest/config'
 import { config } from 'dotenv'
 
 export default defineConfig({
+  define: {
+    __SDK_VERSION__: JSON.stringify(process.env.npm_package_version ?? '0.1.0'),
+  },
   test: {
     dir: 'tests',
     testTimeout: 10000,
