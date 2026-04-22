b0e62865914a884a426d0435f516b796ca005fc9
Radim Höfer <radim.engineering@stashbase.dev>
2026-04-22 08:16:16 +0200

    chore: remove sdkDevApiUrl variable from tsup config

--- tsup.config.ts
+++ tsup.config.ts
@@ -11,7 +11,6 @@ const packageJson = JSON.parse(
 ) as PackageJson
 
 const sdkVersion = packageJson.version ?? '0.0.0'
-const sdkDevApiUrl = process.env.STASHBASE_SDK_DEV_API_URL ?? ''
 
 export default defineConfig({
   entry: ['src/index.ts'],
@@ -19,6 +18,6 @@ export default defineConfig({
   dts: true,
   define: {
     __SDK_VERSION__: JSON.stringify(sdkVersion),
-    __SDK_DEV_API_URL__: JSON.stringify(sdkDevApiUrl),
+    __SDK_DEV_API_URL__: JSON.stringify(''),
   },
 })
