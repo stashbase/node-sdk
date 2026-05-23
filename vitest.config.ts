b5ec57e357ac1c3b6b777d7ce5fef3cf0172bb3f
Radim Höfer <radim.engineering@stashbase.dev>
2026-05-23 13:59:28 +0200

    refactor: streamline environment variable loading in vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,18 +1,20 @@
 import { defineConfig } from 'vitest/config'
 import { config } from 'dotenv'
 
+const env = {
+  ...config({ path: './.env' }).parsed,
+  ...config({ path: './env' }).parsed,
+}
+
 export default defineConfig({
   define: {
     __SDK_VERSION__: JSON.stringify(process.env.npm_package_version ?? '0.1.0'),
-    __SDK_DEV_API_URL__: JSON.stringify(process.env.STASHBASE_SDK_DEV_API_URL ?? ''),
+    __SDK_DEV_API_URL__: JSON.stringify(env.STASHBASE_SDK_DEV_API_URL ?? ''),
   },
   test: {
     dir: 'tests',
     testTimeout: 10000,
-    env: {
-      ...config({ path: './.env' }).parsed,
-      ...config({ path: './env' }).parsed,
-    },
+    env,
   },
   server: {
     port: 3000,
