6c8312b1b2a181a2ac3be5d9d98b7e9d2da0c861
Radim Höfer <radim.engineering@stashbase.dev>
2026-05-23 16:14:52 +0200

    fix: update createWebhook function to use WebhookWithSigningSecret type

--- src/api/environments/handlers/webhooks/create.ts
+++ src/api/environments/handlers/webhooks/create.ts
@@ -1,13 +1,13 @@
 import { HttpClient } from '../../../../http/client'
-import { CreateWebhookData, CreateWebhookResponse } from '../../../../types/webhooks'
+import { CreateWebhookData, WebhookWithSigningSecret } from '../../../../types/webhooks'
 import { CreateWebhookErrorCode } from '../../../../types/errors/webhooks'
 import { ApiResponse } from '../../../../http/response'
 
 async function createWebhook(
   envClient: HttpClient,
   data: CreateWebhookData
-): Promise<ApiResponse<CreateWebhookResponse, CreateWebhookErrorCode>> {
-  return await envClient.sendApiRequest<CreateWebhookResponse, CreateWebhookErrorCode>({
+): Promise<ApiResponse<WebhookWithSigningSecret, CreateWebhookErrorCode>> {
+  return await envClient.sendApiRequest<WebhookWithSigningSecret, CreateWebhookErrorCode>({
     method: 'POST',
     path: '/v1/environment/webhooks',
     data,
